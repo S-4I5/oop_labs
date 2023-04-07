78bd45c2e8d45aedce10fc7af444b85721e64d6c
S-4I5 <yasha.poluektovv@gmail.com>
2023-04-08 01:51:50 +0300

    lab2 + lab5 finished

--- src/main/java/org/example/lab2/Main.java
+++ src/main/java/org/example/lab2/Main.java
@@ -4,23 +4,27 @@ import lombok.SneakyThrows;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public final class Main {
-    private static final ExampleClass TEST_CLASS = new ExampleClass();
-
-    @SneakyThrows
+    private static final ExampleClass EXAMPLE_CLASS = new ExampleClass();
     private static <T> void invokeMethodCertainAmountOfTimes(Method method, int amountOfCalls, T argument) {
         for (int i = 0; i < amountOfCalls; i++) {
-            method.invoke(TEST_CLASS, argument);
+            try {
+                method.invoke(EXAMPLE_CLASS, argument);
+            } catch (IllegalAccessException | InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
     public static void main(String[] args) {
-        ArrayList<Method> methods = Arrays.stream(ExampleClass.class.getDeclaredMethods())
-                .filter(method -> method.isAnnotationPresent(MyAnnotation.class))
+        ArrayList<Method> methods = Arrays.stream(EXAMPLE_CLASS.getClass().getDeclaredMethods())
+                .filter(method -> !Modifier.isPublic(method.getModifiers()) &&
+                        method.isAnnotationPresent(MyAnnotation.class))
                 .collect(Collectors.toCollection(ArrayList::new));
 
         methods.forEach(method ->{
@@ -33,7 +37,7 @@ public final class Main {
                 invokeMethodCertainAmountOfTimes(method, amountOfCallsInAnnotation, 1);
             } else if (argumentClass.equals(String.class)) {
                 invokeMethodCertainAmountOfTimes(method, amountOfCallsInAnnotation, "xd");
-            } else if (argumentClass.equals(Boolean.class)) {
+            } else if (argumentClass.equals(boolean.class)) {
                 invokeMethodCertainAmountOfTimes(method, amountOfCallsInAnnotation, false);
             }
 
--- src/main/java/org/example/lab5/Main.java
+++ src/main/java/org/example/lab5/Main.java
@@ -1,32 +1,103 @@
 package org.example.lab5;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
-    private static final ArrayList<Integer> EXAMPLE_INT_ARRAY = new ArrayList<>(List.of(1,2,3,4,5));
-    private static final ArrayList<String> EXAMPLE_STRING_ARRAY = new ArrayList<>(List.of(
-            "Amogus",
-            "SYS",
-            "XDDDD"));
+    private static final ArrayList<Integer> EXAMPLE_INT_ARRAY = new ArrayList<>(
+            List.of( 1, 2, 2, 3, 4, 5)
+    );
+    private static final ArrayList<String> EXAMPLE_STRING_ARRAY = new ArrayList<>(
+            List.of("Amogus", "Aaogus", "SYS", "XDDDD")
+    );
 
-    public double getListAverage(ArrayList<Integer> list){
+    public static double getListAverage(ArrayList<Integer> list){
         return list.stream().mapToInt(x -> x)
                 .average().orElse(0);
     }
 
-    public ArrayList<String> getListInUpperCase(ArrayList<String> list){
+    public static ArrayList<String> getListInUpperCase(ArrayList<String> list){
         return list.stream().map(x -> x.toUpperCase().concat("_new_"))
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 
-    /*public ArrayList<Integer> getSquareOfUniqueElements(ArrayList<Integer> list){
-        return list.stream().filter(x -> list.
-    }*/
+    public static ArrayList<Integer> getSquareOfUniqueElements(ArrayList<Integer> list){
+        return list.stream()
+                .filter(x -> Collections.frequency(list, x) == 1)
+                .map(x -> x*x)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
 
+    public static ArrayList<String> getStringsWithLetterOnFirstPosition(ArrayList<String> list, char element){
+        return list.stream()
+                .filter(x -> x.charAt(0) == element)
+                .sorted()
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public static <T> T getLastElement(ArrayList<T> list){
+        return list.stream()
+                .reduce((x,y) -> y)
+                .orElseThrow(RuntimeException::new);
+    }
+
+    public static int getSumOfEvenElements(ArrayList<Integer> list){
+        return list.stream()
+                .mapToInt(x -> x).filter(x -> x%2 == 0)
+                .sum();
+    }
+
+    public static Map<Character, String> listToMap(ArrayList<String> list){
+        return list.stream()
+                .filter(x -> !x.isEmpty())
+                .collect(Collectors.toMap(
+                        x -> x.charAt(0),
+                        x -> x,
+                        (o1, o2) -> o1 + " " + o2));
+    }
 
     public static void main(String[] args) {
+        System.out.println("1) Среднее значение списка целых чисел");
+        System.out.println("Среднее значение EXAMPLE_INT_ARRAY: " + getListAverage(EXAMPLE_INT_ARRAY));
 
+        System.out.println("2) Верхний регистр + _new_: ");
+        System.out.println("Было:");
+        EXAMPLE_STRING_ARRAY.forEach(x -> System.out.print(x + " "));
+        System.out.println();
+        System.out.println("Стало:");
+        getListInUpperCase(EXAMPLE_STRING_ARRAY).forEach(x -> System.out.print(x + " "));
+        System.out.println();
+
+        System.out.println("3) Список квадратов уникальных элементов: ");
+        System.out.println("Было:");
+        EXAMPLE_INT_ARRAY.forEach(x -> System.out.print(x + " "));
+        System.out.println("Стало:");
+        getSquareOfUniqueElements(EXAMPLE_INT_ARRAY).forEach(x -> System.out.print(x + " "));
+        System.out.println();
+
+        System.out.println("4) Поиск строк по первому символу: ");
+        System.out.println("Было:");
+        EXAMPLE_STRING_ARRAY.forEach(x -> System.out.print(x + " "));
+        System.out.println();
+        System.out.println("Стало:");
+        getStringsWithLetterOnFirstPosition(EXAMPLE_STRING_ARRAY, 'A').forEach( x -> System.out.print(x + " "));
+        System.out.println();
+
+        System.out.println("5) Последний элемент или исключение: " + getLastElement(EXAMPLE_STRING_ARRAY));
+        System.out.print("Пример с пустым массивом: ");
+        try {
+            System.out.println(getLastElement(new ArrayList<String>()));
+        } catch (RuntimeException exception){
+            System.out.println("Пустой массив");
+        }
+
+        System.out.println("6) Сумма чётных чисел: " + getSumOfEvenElements(EXAMPLE_INT_ARRAY));
+        System.out.println(" Пример с пустым массивом: " + getSumOfEvenElements(new ArrayList<Integer>()));
+
+        System.out.println("7) метод, преобразовывающий все строки в списке в Map, гдe первый символ – ключ, оставшиеся – значение: ");
+        System.out.println(listToMap(EXAMPLE_STRING_ARRAY));
     }
 }
